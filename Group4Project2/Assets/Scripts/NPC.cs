fbba1cac1007cb583c09ef0750143627654d9603
Xmayer-72 <conrad.mayer72@gmail.com>
2022-03-30 16:28:00 -0500

    tweaks

--- Group4Project2/Assets/Scripts/NPC.cs
+++ Group4Project2/Assets/Scripts/NPC.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class NPC : Interactables
 {
-    [SerializeField] private GameObject Speech;
-    private Text SpeechText;
+    [SerializeField] private GameObject Speech; private Text SpeechText;
+
     [SerializeField] private int NPCState = 0;
+
+    //array of statements to be displayed when interacted with
     public string[] Sentances;
 
     private void Awake()
@@ -20,18 +22,22 @@ public class NPC : Interactables
     public override void Interact()
     {
         //On interact with NPC, talk to npc in its current state
-       StartCoroutine(Talk(NPCState));
+        StartCoroutine(Talk(NPCState));
     }
 
+    //triggers display of the current response depending on status
     public IEnumerator Talk(int status)
     {
-        //triggers display of the current response depending on status
+        //sets the speech text
         SpeechText.text = Sentances[status];
 
+        //shows speech bubble
         Speech.SetActive(true);
 
+        //waits 4 seconds so can be read by player
         yield return new WaitForSeconds(4);
 
+        //hides speech bubble
         Speech.SetActive(false);
     }
 }
