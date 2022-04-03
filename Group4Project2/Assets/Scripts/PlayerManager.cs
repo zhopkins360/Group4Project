ddac666cb9581298f3402101bdc60cdd8e737d20
Zackary Hopkins <zhopkins@mail.bradley.edu>
2022-04-02 20:17:11 -0500

    Added Inventory UI element
    Made an Inventory with a few buttons. Pressing I will toggle the inventory. The UI is now rendered in it's own Camera so the 3D icons could be used

--- Group4Project2/Assets/Scripts/PlayerManager.cs
+++ Group4Project2/Assets/Scripts/PlayerManager.cs
@@ -7,9 +7,12 @@ public class PlayerManager : MonoBehaviour
 {
     public int maxNumberOfActions = 3;
     public Slider actionBar;
+    public Item[] backPack;
+    GameObject Inventory;
     // Start is called before the first frame update
     void Start()
     {
+        Inventory = GameObject.FindGameObjectWithTag("Inventory");
         actionBar = GameObject.FindGameObjectWithTag("ActionBar").GetComponent<Slider>();
         actionBar.maxValue = maxNumberOfActions;
         actionBar.value = maxNumberOfActions;
@@ -26,8 +29,13 @@ public class PlayerManager : MonoBehaviour
         {
             resetActions();
         }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Inventory.SetActive(!Inventory.activeSelf);
+        }
     }
 
+    //Code for interacting with the players stamina 
     public void useAction()
     {
         actionBar.value -= 1;
@@ -36,5 +44,25 @@ public class PlayerManager : MonoBehaviour
     public void resetActions()
     {
         actionBar.value = maxNumberOfActions;
+    }
+
+
+    //code for interacting with the player back pack
+    public void pickUpItem()
+    {
+        int freeIndex = -1;
+        for(int i = 0; i < backPack.Length; i++)
+        {
+            if(backPack[i] == null)
+            {
+                freeIndex = i;
+            }
+        }
+
+    }
+
+    public void dropItem(int id)
+    {
+
     }
 }
