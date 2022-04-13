3e49361bfab3c2865cb07b8978ad529af7726549
Xmayer-72 <conrad.mayer72@gmail.com>
2022-04-13 12:22:18 -0500

    Separated talk functions into default and overrule

--- Group4Project2/Assets/Scripts/NPC.cs
+++ Group4Project2/Assets/Scripts/NPC.cs
@@ -26,32 +26,12 @@ public class NPC : Interactables
     public override void Interact()
     {
         //On interact with NPC, talk to npc in its current state
-        StartCoroutine(Talk(NPCState));
+        StartCoroutine(Talk());
     }
 
-    //triggers display of the current response depending on status
+    //displays text based on provided status
     public IEnumerator Talk(int status)
     {
-        //states:
-        //state 0: NPC has never been spoken to
-        //state 1: NPC has since informed you of the requested item
-        //state 2: Player completes request
-        //state 3: Player has already completed request
-
-        //when in certain states, energy shouldn't be used, especially since the NPC will just repeat himself
-        if (status == 1 || status == 3)
-        {
-            PlayerManager.Instance.actionBar.value += 1;
-        }
-
-        //check if wanted item is in inventory
-        if (Backpack.Instance.IsObjectInBackpack(wantedItem.GetComponent<Collectable>().ID) && status == 1)
-        {
-            NPCState = 2;
-            status = 2;
-        }
-
-
         if (status < Sentances.Length)
         {
             //sets the speech text
@@ -62,14 +42,57 @@ public class NPC : Interactables
             SpeechText.text = "<b>" + label + "</b>\n" + "Current status does not have any text associated with it. Status #: " + status;
         }
 
+        //shows speech bubble
+        Speech.SetActive(true);
+
+        //waits 4 seconds so can be read by player
+        yield return new WaitForSeconds(4);
+
+        //hides speech bubble
+        Speech.SetActive(false);
+    }
+
+    //triggers display of the current response depending on current NPCState
+    public IEnumerator Talk()
+    {
+        //states:
+        //state 0: NPC has never been spoken to
+        //state 1: NPC has since informed you of the requested item
+        //state 2: Player completes request
+        //state 3: Player has already completed request
+
+        //when in certain states, energy shouldn't be used, especially since the NPC will just repeat himself
+        if (NPCState == 1 || NPCState == 3)
+        {
+            PlayerManager.Instance.actionBar.value += 1;
+        }
+
+        //check if wanted item is in inventory
+        if (Backpack.Instance.IsObjectInBackpack(wantedItem.GetComponent<Collectable>().ID) && NPCState == 1)
+        {
+            NPCState = 2;
+            NPCState = 2;
+        }
+
+
+        if (NPCState < Sentances.Length)
+        {
+            //sets the speech text
+            SpeechText.text = "<b>" + label + "</b>\n" + Sentances[NPCState];
+        }
+        else
+        {
+            SpeechText.text = "<b>" + label + "</b>\n" + "Current status does not have any text associated with it. Status #: " + NPCState;
+        }
+
         //advance states appropriately
         //if in state 0, npc has now been spoken to, advance state
-        if (status == 0)
+        if (NPCState == 0)
         {
             AdvanceState();
         }
         //if in state 2, npc has now acknowledged the fulfilled request, so advance state
-        else if (status == 2)
+        else if (NPCState == 2)
         {
             AdvanceState();
         }
