168da37504db5f65db37fd6752a42d421bb95107
Zackary Hopkins <zhopkins@mail.bradley.edu>
2022-05-10 18:54:14 -0500

    fix dumb

--- Group4Project2/Assets/Scripts/TutorialDoor.cs
+++ Group4Project2/Assets/Scripts/TutorialDoor.cs
@@ -5,13 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TutorialDoor : Door
 {
-    [Header("Scene to be loaded by door \nWARNING: only add scene references here")]
-    public Object MainScene;
-
     public override void Interact()
     {
-        string name = MainScene.name;
         StartCoroutine(BlackoutScreen());
-        SceneManager.LoadScene("DevScene");
+        SceneManager.LoadScene("DevScene", LoadSceneMode.Single);
     }
 }
