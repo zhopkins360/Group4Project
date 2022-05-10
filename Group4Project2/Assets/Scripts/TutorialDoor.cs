503cd285afdc0ccd8be6c2a2ae21de9d1661dd55
Xmayer-72 <conrad.mayer72@gmail.com>
2022-05-10 18:54:09 -0500

    Fixes (cont.)

--- Group4Project2/Assets/Scripts/TutorialDoor.cs
+++ Group4Project2/Assets/Scripts/TutorialDoor.cs
@@ -3,15 +3,106 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TutorialDoor : Door
+public class TutorialDoor : Interactables
 {
     [Header("Scene to be loaded by door \nWARNING: only add scene references here")]
     public Object MainScene;
 
+    //fade bools
+    public bool fadeIn = false;
+    public bool fadeOut = false;
+
+    //reference to blackout UI
+    private CanvasGroup blackout;
+
+    //playerManager reference
+    private PlayerManager playerManager;
+
+    private void Awake()
+    {
+        //set outline reference
+        outlines = GetComponents<cakeslice.Outline>();
+
+        //if outlines are not found, look in childeren
+        if (outlines.Length == 0)
+        {
+            outlines = GetComponentsInChildren<cakeslice.Outline>();
+        }
+
+        //get manager instance
+        if (PlayerManager.IsInstantilized)
+        {
+            playerManager = PlayerManager.Instance;
+        }
+        else
+        {
+            playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+        }
+
+        //sets blackout UI reference
+        blackout = GameObject.FindGameObjectWithTag("BlackOut").GetComponent<CanvasGroup>();
+    }
+
     public override void Interact()
     {
-        string name = MainScene.name;
+        //string name = MainScene.name;
         StartCoroutine(BlackoutScreen());
-        SceneManager.LoadScene("DevScene");
+        SceneManager.UnloadSceneAsync("TutorialScene");
+        SceneManager.LoadSceneAsync(MainScene.name, LoadSceneMode.Additive);
+    }
+
+    protected IEnumerator BlackoutScreen()
+    {
+        //initiates blackout
+        fadeIn = true;
+
+        //wait for transition
+        yield return new WaitForSeconds(1.5f);
+
+        //set day to next day
+        playerManager.Day++;
+
+        //wait for day change
+        yield return new WaitForSeconds(0.5f);
+
+        //disable blackout
+        fadeOut = true;
+    }
+
+    void FixedUpdate()
+    {
+        //if fading in
+        if (fadeIn)
+        {
+            //if blackout is not opaque
+            if (blackout.alpha < 1)
+            {
+                //add deltaTime to alpha
+                blackout.alpha += Time.deltaTime;
+
+                //once alpha = 1, stop fadein
+                if (blackout.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+
+        //if fading out
+        if (fadeOut)
+        {
+            //if blackout is visible
+            if (blackout.alpha >= 0)
+            {
+                //subtract deltatime from alpha
+                blackout.alpha -= Time.deltaTime;
+
+                //once is not visable, stop fadout
+                if (blackout.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
     }
 }
